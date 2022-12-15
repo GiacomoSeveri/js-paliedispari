c3e250191c4e90121b4fed217d8ad2ff657c11b2
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-15 16:09:49 +0100

    problemi

---
+++ palindromo/js/script.js
@@ -0,0 +1,33 @@
+
+
+const textWord = document.getElementById('txt');
+const btn = document.getElementById('button');
+const result = document.getElementById('result');
+
+btn.addEventListener('click', function(){
+
+    const currentWord = textWord.value.trim();
+
+    if(!currentWord) {
+        alert('Parola non valida');
+        return;
+    }
+
+    function reverseWord(word) {
+        let reverse = ' ';
+
+        for(let i = word.length - 1; i >= 0; i--) {
+            reverse += word[i];
+        }
+        return reverse;
+    }
+    
+    const reversedWord = reverseWord(currentWord);
+    console.log(reversedWord);
+
+    if(currentWord == reversedWord) {
+        result.innerText = `Si, ${currentWord} è palindroma`;
+    } else {
+        result.innerText = `No, ${currentWord} non è palindroma`;
+    }
+});
